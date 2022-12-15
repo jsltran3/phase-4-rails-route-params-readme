888ec27eb453399804f5af97ae63d6cabe2124e1
Joseph <jsltran@gmail.com>
2022-12-15 14:49:25 -0800

    commit tree 599d66e61bc50c0ba53bd86a391556b771f89e36

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -5,4 +5,13 @@ class CheesesController < ApplicationController
     render json: cheeses
   end
 
+  def show
+    
+    #find a cheese using id from url 
+    #send json response using that cheese object 
+    #to access special information as part of this dynamic route and that's params 
+    cheese = Cheese.find_by(id: params[:id])
+    render json: cheese
+  end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get '/cheeses', to: 'cheeses#index'
+  get '/cheeses/:id', to: 'cheeses#show'
+  #the above is from the application controller action?
+  
 end
